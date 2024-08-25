e56cc05721b5401a6354949f5f3f6cfb5db2ffca
HHS <qazplm1021@naver.com>
2024-08-25 17:45:33 +0900

    first commit

---
+++ counseling-cms/src/main/java/com/counseling/cms/CounselingCmsApplication.java
@@ -0,0 +1,13 @@
+package com.counseling.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounselingCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounselingCmsApplication.class, args);
+	}
+
+}
---
+++ counseling-cms/src/test/java/com/counseling/cms/counseling_cms/CounselingCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.counseling.cms.counseling_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounselingCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
