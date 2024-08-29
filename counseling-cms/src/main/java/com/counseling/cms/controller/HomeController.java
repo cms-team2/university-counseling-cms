6b3f03821f2bd746e4f1ab3279440c36198b28d9
seeungirl <seeungirl99@gmail.com>
2024-08-29 21:16:06 +0900

    2024-08-29 header

--- counseling-cms/src/main/java/com/counseling/cms/controller/HomeController.java
+++ counseling-cms/src/main/java/com/counseling/cms/controller/HomeController.java
@@ -6,8 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/")
+	@GetMapping("/welcome")
 	public String homePage() {
 		return "welcome";
 	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "/index.html";
+	}
+	
+	@GetMapping("/user/login")
+	public String userLoginPage() {
+		return "/user/login.html";
+	}
+	
+	@GetMapping("/user/pw/find")
+	public String userFindPassword() {
+		return "/user/pw/find.html";
+	}
+	
+	@GetMapping("/user/pw/change")
+	public String userChangePassword() {
+		return "/user/pw/change.html";
+	}
+	
 }
