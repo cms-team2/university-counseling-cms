43417fc7aee28fcd085abe0eb73e333475b2b180
seeungirl <seeungirl99@gmail.com>
2024-09-21 13:09:03 +0900

    09212

--- counseling-cms/src/main/java/com/counseling/cms/controller/AdminChatController.java
+++ counseling-cms/src/main/java/com/counseling/cms/controller/AdminChatController.java
@@ -6,11 +6,12 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class AdminChatController {
-	
+
 	@MessageMapping("/admin/sendMessage")
 	@SendTo("/counselor/messages")
 	public String sendMessage(@Payload Map<String, String> message) {
