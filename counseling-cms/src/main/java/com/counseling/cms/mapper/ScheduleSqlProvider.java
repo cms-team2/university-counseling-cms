89e13dbe66ff46e42c38b851ebeb5c9400d6aeab
chgyeol <ch.gyeol95@gmail.com>
2024-09-25 19:46:50 +0900

    20240925 chatbot

--- counseling-cms/src/main/java/com/counseling/cms/mapper/ScheduleSqlProvider.java
+++ counseling-cms/src/main/java/com/counseling/cms/mapper/ScheduleSqlProvider.java
@@ -18,36 +18,36 @@ public class ScheduleSqlProvider {
 				+ "JOIN STDNT_INFO AS s ON d.STDNT_NO = s.STDNT_NO "
 				+ "JOIN DSCSN_CATEGORY AS c ON d.C_SCLSF_NM = c.C_SCLSF_NM "
 				+ "JOIN EMP_INFO AS e ON d.EMP_NO = e.EMP_NO "
-				+ "WHERE d.DSCSN_YN='Y' AND c.C_SCLSF_NM<>'교수상담' ");
+				+ "WHERE d.DSCSN_YN='Y' AND c.C_SCLSF_NM<>'교수상담'");
 		
         //검색 유형 조건
 		if(search_type==null || search_value==null || search_type.equals("") || search_value.equals("")) {
 			
 		}
 		else if("student".equals(search_type)) {
-        	sql.append("AND s.FLNM LIKE CONCAT('%', #{search_value}, '%') ");
+        	sql.append(" AND s.FLNM LIKE CONCAT('%', #{search_value}, '%')");
         }else if("counselor".equals(search_type)) {
-        	sql.append("AND e.FLNM LIKE CONCAT('%', #{search_value}, '%') ");
+        	sql.append(" AND e.FLNM LIKE CONCAT('%', #{search_value}, '%')");
         }
         
         //정렬 조건 추가
         if (status == null || status.equals("")) {
         }else if(status.equals("counseling_date")) {
-        	sql.append("ORDER BY DSCSN_RSVT_YMD DESC "); //상담예정일 내림차순 정렬
+        	sql.append(" ORDER BY DSCSN_RSVT_YMD DESC"); //상담예정일 내림차순 정렬
         }
         else if ("student_name".equals(status)) {
-            sql.append("ORDER BY FLNM ASC "); // 이름순 정렬
+            sql.append(" ORDER BY FLNM ASC"); // 이름순 정렬
         }
         
         //페이지 보이기 조건 추가
         if(pageno==null) {
-        	sql.append("LIMIT 0,${ea}");
+        	sql.append(" LIMIT 0,${ea}");
         }
         else if(pageno==1) {
-        	sql.append("LIMIT 0,${ea}");
+        	sql.append(" LIMIT 0,${ea}");
         }
         else {
-        	sql.append("LIMIT ${(pageno-1)*ea},${ea}");
+        	sql.append(" LIMIT ${(pageno-1)*ea},${ea}");
         }
         
         return sql.toString();
@@ -59,18 +59,18 @@ public class ScheduleSqlProvider {
 		sql.append("SELECT COUNT(*) "
 				+ "FROM DSCSN_APLY_INFO AS d "
 				+ "JOIN STDNT_INFO AS s ON d.STDNT_NO = s.STDNT_NO "
-				+ "JOIN DSCSN_CATEGORY AS c ON d.C_SCLSF_NM = c.C_SCLSF_CD "
+				+ "JOIN DSCSN_CATEGORY AS c ON d.C_SCLSF_NM = c.C_SCLSF_NM "
 				+ "JOIN EMP_INFO AS e ON d.EMP_NO = e.EMP_NO "
-				+ "WHERE d.DSCSN_YN='Y' AND c.C_SCLSF_NM<>'교수상담' ");
+				+ "WHERE d.DSCSN_YN='Y' AND c.C_SCLSF_NM<>'교수상담'");
 		
         //검색 유형 조건
 		if(search_type==null || search_value==null || search_type.equals("") || search_value.equals("")) {
 			
 		}
 		else if("student".equals(search_type)) {
-        	sql.append("AND s.FLNM LIKE CONCAT('%', #{search_value}, '%') ");
+        	sql.append(" AND s.FLNM LIKE CONCAT('%', #{search_value}, '%') ");
         }else if("counselor".equals(search_type)) {
-        	sql.append("AND e.FLNM LIKE CONCAT('%', #{search_value}, '%') ");
+        	sql.append(" AND e.FLNM LIKE CONCAT('%', #{search_value}, '%') ");
         }
 
 		return sql.toString();
